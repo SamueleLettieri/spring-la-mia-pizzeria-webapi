fed368aeae5bbccb85908d76fc24eb888c83a64f
SamueleLettieri <samuele2073@gmail.com>
2023-02-27 14:54:12 +0100

    stampate a schermo tutte le pizze

---
+++ frontEnd/myScript.js
@@ -0,0 +1,34 @@
+elencoPizze();
+
+
+function elencoPizze(){
+    axios.get("http://localhost:8080/api/pizze")
+    .then((result) => {
+        console.log(result.data)
+        result.data.forEach(pizze => {
+            console.log(pizze);
+            document.querySelector("#pizze").innerHTML += `
+            <tr >
+                <td > <a href="./detail.html?id=${pizze.id}">${pizze.name}</a></td>
+                <td> ${pizze.description}</td>
+                <td >
+                    <img alt="logo" src="${pizze.photo}" class="w-50" >
+                </td>
+                <td>${pizze.price}€</td>
+                <td >
+                    <a href=""  class="btn btn-primary">Modifica</a>
+                    <a href="" class="btn btn-danger">Elimina</a>
+                </td>
+            </tr>
+            `;
+        });
+    }).catch((error) => {
+        console.warn(error)
+    })
+    
+}
+
+
+
+
+
